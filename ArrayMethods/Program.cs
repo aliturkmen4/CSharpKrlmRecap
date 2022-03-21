a6963f11a9ff8e5df574e41b230187cce9dcecf0
Ali <alym11731996@gmail.com>
2022-03-21 12:50:39 +0300

    Arrays were updated

---
+++ ArrayMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
