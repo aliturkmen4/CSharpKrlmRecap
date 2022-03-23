549b938abb18f66a0779a56acc38fd40cc5765b4
Ali <alym11731996@gmail.com>
2022-03-23 17:51:36 +0300

    exceptionhandling was repeated

---
+++ ExceptionanExceptionHandling/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ExceptionandExceptionHandling
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exception: Programı yazarken öngöremediğimiz hatalardır!
+
+
+            //Unhandled exception. System.FormatException
+
+            //Unhandled exception. System.DivideByZeroException
+
+            //Unhandled exception. System.IndexOutofRangeExcepiton
+
+            //Unhandled exception. System.NullReferenceExcepiton  
+
+            //Exception Handling: Hataya karşı bir önlem almak!
+
+            try
+            {
+                Console.Write("a:");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(" ");
+
+                Console.Write("b:");
+                int b = int.Parse(Console.ReadLine());
+
+                var sonuc = a / b;
+                Console.WriteLine("{0} / {1}={2}", a, b, sonuc);
+                //b ye 0 verirsen dividebyzero hatası alırsın!
+            }
+            catch (DivideByZeroException ex) //exception handling kısmı
+            {
+                Console.WriteLine("b sıfır olamaz!");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex) //exception handling kısmı
+            {
+                Console.WriteLine("sayısal bilgi girmelisiniz!");
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Bir hata oluştu!");
+                Console.WriteLine(ex.Message);
+            }
+            finally //hata olsun ya da olmasın çalıştırılacak kod bloğunu buraya yaz
+            {
+                Console.WriteLine("finally bloğu çalıştı!");
+            }
+
+        }
+    }
+}
