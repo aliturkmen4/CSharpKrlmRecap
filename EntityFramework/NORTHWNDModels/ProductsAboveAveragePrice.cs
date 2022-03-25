62c9a8ef163cd7ac6815f0e08f828eebffe78caf
Ali <alym11731996@gmail.com>
2022-03-26 02:21:18 +0300

    Northwind db added the ef project via scoffolding

---
+++ EntityFramework/NORTHWNDModels/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFramework.NORTHWNDModels
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
---
+++ EntityFramework/NORTHWNDModels/SalesByCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFramework.NORTHWNDModels
+{
+    public partial class SalesByCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal? ProductSales { get; set; }
+    }
+}
